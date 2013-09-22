77ee5737e53687a77194f0aad45eda26ef9c975e
Dario Magliocchetti <dario_dml@hotmail.com>
2013-09-22 11:34:33 +0100

    initial commit

--- header.php
+++ header.php
@@ -46,9 +46,13 @@
 			<header class="header" role="banner">
 
 				<div id="inner-header" class="wrap clearfix">
-
 					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<p id="logo" class="h1">
+						<img width="100px" src="<?php echo get_template_directory_uri(); ?>/library/images/cgcu.png">
+						<a href="<?php echo home_url(); ?>" rel="nofollow">
+							<?php bloginfo('name'); ?>
+						</a>
+					</p>
 
 					<!-- if you'd like to use the site description you can un-comment it below -->
 					<?php // bloginfo('description'); ?>
